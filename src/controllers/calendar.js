32b36bb46295b39ca0d674550eee94c8106ba14e
Johann Schwarze <johannschwarze@Air-de-Johann.fritz.box>
2022-09-20 13:57:00 +0200

    setting aws ses

--- src/controllers/calendar.js
+++ src/controllers/calendar.js
@@ -1,4 +1,8 @@
+import  {sendMailTest}  from "../lib/mailer.js";
+
 export const create = async (req, res, next) => {
     console.log('wena wena')
+    sendMailTest()
+
     res.send('wena wena')
 };
---
+++ src/lib/mailer.js
@@ -0,0 +1,47 @@
+import { SEND_EMAIL } from "../services/amazon_ses.js";
+import AWS from "aws-sdk";
+
+var ses = new AWS.SES();
+export const sendMail = async (req, res, next) => {
+  console.log("--Send Email--")
+  console.log(req.body);
+
+  let EMAIL_DATA = {
+    cc: req.body.cc, // ARRAY
+    to: req.body.to, //ARRAY
+    replyTo: req.body.replyTo, //ARRAY
+    template: req.body.template, // STRING
+    templateData: req.body.templateData, // STRING
+    source: req.body.source, //STRING
+  };
+
+  let resp = await SEND_EMAIL(EMAIL_DATA);
+  res.send(resp);
+};
+export const getTemplate = async (req, res, next) => {
+  let params = {
+    TemplateName: req.params.template/* required */
+  };
+  ses.getTemplate(params, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else  res.send(data.Template);;           // successful response
+  });
+};
+
+export const sendMailTest = async (req, res, next) => {
+    console.log("--Send Email test--")
+  
+    let EMAIL_DATA = {
+        cc: ['jschacosta@gmail.com'],
+      // to: ["jschacosta@gmail.com", "stefano.celsi.12@sansano.usm.cl"],
+      to: ["jschacosta@gmail.com"],
+      replyTo: [],
+      template: 'index',
+      templateData:"{\"user_name\":\"" + ' ' + "\"}",
+      source: 'johann <info@johannsch.pro>',
+      };
+  
+    let resp = await SEND_EMAIL(EMAIL_DATA);
+    // res.send(resp);
+    console.log(resp)
+  };

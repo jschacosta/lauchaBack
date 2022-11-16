ef6c519331bdbcbc6a211c6bf032e44a2d5f1dbd
Johann Schwarze <johannschwarze@Air-de-Johann.fritz.box>
2022-11-16 19:44:30 +0100

    change port

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import server from './server.js';
 
-
-server.listen(3000, function () {
-  console.log('Barber App Api is listening on port 3000!');
+const port = process.env.PORT || 3000;
+server.listen(port, function () {
+  console.log('Barber App Api is listening on port '+ port);
 });

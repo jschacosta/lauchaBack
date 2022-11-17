88971cda09d6f1d6177476c3e284856663a8ef5b
Johann Schwarze <johannschwarze@Air-de-Johann.fritz.box>
2022-11-17 01:43:32 +0100

    cambios back

--- src/server.js
+++ src/server.js
@@ -39,9 +39,9 @@ app.options("*", cors());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  // res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-  // res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  // res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
 
   next();
 });
@@ -55,7 +55,7 @@ app.use(bodyParser.json());
 
 app.use("/", routes);
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Barber Api works!');
 });
 // Middleware para Vue.js router modo history
 app.use(history());

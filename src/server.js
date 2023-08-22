e09b5901f5b254f8ac07e7ee0db3abbf36186622
JohannSch <jschacosta@gmail.com>
2023-08-23 01:06:13 +0200

    mejora html

--- src/server.js
+++ src/server.js
@@ -56,16 +56,80 @@ app.use(bodyParser.json());
 app.use("/", routes);
 app.get('/', (req, res) => {
   const htmlResponse = `
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <title>SOS-API</title>
-      </head>
-      <body>
-        <h1>Welcome to the SOS API TRAVELERS</h1>
-        <p>This is a more elaborate HTML response</p>
-      </body>
-    </html>
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SOS-API</title>
+    <style>
+      body {
+        font-family: Arial, sans-serif;
+        margin: 0;
+        padding: 0;
+        background-color: #f5f5f5;
+      }
+  
+      .container {
+        width: 80%;
+        max-width: 1200px;
+        margin: 0 auto;
+        padding: 20px;
+        background-color: #fff;
+        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+      }
+  
+      header {
+        text-align: center;
+        margin-bottom: 40px;
+      }
+  
+      h1 {
+        color: #333;
+        margin-bottom: 10px;
+      }
+  
+      p {
+        color: #777;
+        font-size: 18px;
+      }
+  
+      .api-description {
+        margin-top: 30px;
+      }
+  
+      .cta-button {
+        display: inline-block;
+        padding: 10px 20px;
+        background-color: #007bff;
+        color: #fff;
+        text-decoration: none;
+        border-radius: 5px;
+        font-weight: bold;
+        transition: background-color 0.3s ease;
+      }
+  
+      .cta-button:hover {
+        background-color: #0056b3;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <header>
+        <h1>Welcome to SOS API</h1>
+        <p>Your source for amazing API services.</p>
+      </header>
+      <div class="api-description">
+        <h2>What SOS API Offers:</h2>
+        <p>Our API provides a wide range of functionalities to make your development process smoother and more efficient.</p>
+      </div>
+      <div class="cta">
+        <a href="#" class="cta-button">Get Started</a>
+      </div>
+    </div>
+  </body>
+  </html>
   `;
   res.send(htmlResponse);
 });
